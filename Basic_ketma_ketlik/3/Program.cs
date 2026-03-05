593e2dc9e3aafcc9f5aaee69bd2a309febf1bfc0
Shohijahon Rahmonov <yourgithubemail@gmail.com>
2026-03-05 09:38:25 +0500

    chanche

---
+++ Basic_ketma_ketlik/3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a number: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        if (number <= 1)
+        {
+            Console.WriteLine("Tub emas");
+            return;
+        }
+
+        bool isPrime = true;
+
+        for (int i = 2; i <= Math.Sqrt(number); i++)
+        {
+            if (number % i == 0)
+            {
+                isPrime = false;
+                break;
+            }
+        }
+
+        Console.WriteLine(isPrime ? "Tub" : "Tub emas");
+    }
+}
+
+
