270517c528168185a38983970336168ab0094e03
Shohijahon Rahmonov <yourgithubemail@gmail.com>
2026-03-23 20:20:50 +0500

    last task is finished

---
+++ Basic_Tanlash/task 3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Reflection.Metadata;
+
+class Program
+{
+  static void Main()
+  {
+    Console.Write("So'z kiriting: ");
+    string input = Console.ReadLine();
+
+    // Reverse using built-in method
+    char[] arr = input.ToCharArray();
+    Array.Reverse(arr);
+    string reversed = new string(arr);
+
+    // Check with if-else
+    if (input.ToLower() == reversed.ToLower())
+    {
+      Console.WriteLine("Palindrom");
+    }
+    else
+    {
+      Console.WriteLine("Palindrom emas");
+    }
+
+    string message =
+      (input.ToLower() == reversed.ToLower())
+        ? $"\"{input.ToUpper()}\"- Palindrom!"
+        : $"\"{input.ToUpper()}\"- Not Palindrom!";
+    Console.WriteLine(message);
+  }
+}
---
+++ Basic_ketma_ketlik/4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+  static void Main()
+  {
+    Console.Write("Iltimos, bitta harf kiriting: ");
+    char belgi = Convert.ToChar(Console.ReadLine());
+
+    bool UpperLatter = belgi >= 'A' && belgi <= 'Z';
+    Console.WriteLine(UpperLatter ? "HA" : "YO'Q");
+
+    // task 2
+
+    Console.Write("Dasturlash sizga yoqadimi? (h/y): ");
+    char text = Convert.ToChar(Console.ReadLine());
+
+    bool dastur = text == 'h' || text == 'H';
+    Console.WriteLine(dastur ? "Ajoyib!" : "Afsus!");
+
+    // task  3
+
+    Console.Write("Ismingizni kiriting: ");
+    string ism = Console.ReadLine();
+
+    char firstLatter = ism[0];
+    Console.WriteLine($"{ism}ning birinchi harfi => \"{firstLatter}\" harfiga teng!");
+  }
+}
