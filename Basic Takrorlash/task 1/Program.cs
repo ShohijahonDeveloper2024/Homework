c0ec43c45aae401c50dd3919e36290def7fcfbb7
Shohijahon Rahmonov <yourgithubemail@gmail.com>
2026-03-24 15:44:04 +0500

    .

---
+++ Basic Takrorlash/task 1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+class Programm
+{
+  static void Main()
+  {
+    // task 1
+
+    int number = 1;
+    while (number <= 1001)
+    {
+      Console.WriteLine(number);
+      number += 2;
+    }
+
+    int[] numbers = { 4, 2, 7, 8, 12, 9, 2, 9 };
+    int max = numbers[0];
+    int min = numbers[0];
+    for (int num = 0; num < numbers.Length; num++)
+    {
+      if (numbers[num] > max)
+      {
+        max = numbers[num];
+      }
+      if (numbers[num] < min)
+      {
+        min = numbers[num];
+      }
+    }
+    Console.WriteLine($"{min} x {max} = {min * max}");
+
+    // task 3
+
+    Console.Write("Butun son kiritng: ");
+    int son = Convert.ToInt32(Console.ReadLine());
+
+    int factor = 1;
+
+    for (int i = 1; i <= son; i++)
+    {
+      factor = factor * i;
+    }
+
+    Console.WriteLine($"Natija: {factor}");
+
+  }
+}
+
+
+
+
---
+++ Basic Takrorlash/task 2/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+
+class Program
+{
+  static void Main()
+  {
+    List<string> tasks = new List<string>();
+    List<bool> done = new List<bool>();
+
+    while (true)
+    {
+      Console.WriteLine("\n1 - Vazifa qo'shish");
+      Console.WriteLine("2 - Vazifani o'chirish");
+      Console.WriteLine("3 - Bajarildi deb belgilash");
+      Console.WriteLine("4 - Vazifalarni ko'rish");
+      Console.WriteLine("0 - Chiqish");
+
+      Console.Write("Tanlang: ");
+      int choice = int.Parse(Console.ReadLine());
+
+      if (choice == 1)
+      {
+        Console.Write("Vazifa kiriting: ");
+        string task = Console.ReadLine();
+        tasks.Add(task);
+        done.Add(false);
+      }
+      else if (choice == 2)
+      {
+        Console.Write("O'chiriladigan index: ");
+        int index = int.Parse(Console.ReadLine());
+
+        if (index >= 0 && index < tasks.Count)
+        {
+          tasks.RemoveAt(index);
+          done.RemoveAt(index);
+        }
+      }
+      else if (choice == 3)
+      {
+        Console.Write("Bajarilgan index: ");
+        int index = int.Parse(Console.ReadLine());
+
+        if (index >= 0 && index < done.Count)
+        {
+          done[index] = true;
+        }
+      }
+      else if (choice == 4)
+      {
+        Console.WriteLine("\nVazifalar:");
+
+        for (int i = 0; i < tasks.Count; i++)
+        {
+          string status = done[i] ? "✔" : "❌";
+          Console.WriteLine(i + " - " + tasks[i] + " [" + status + "]");
+        }
+      }
+      else if (choice == 0)
+      {
+        break;
+      }
+    }
+  }
+}
